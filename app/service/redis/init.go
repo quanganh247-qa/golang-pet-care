5ce215012255bdd938febebec0fb23c9ca533ebd
quang-anh-ute <21110752@student.hcmute.edu.vn>
2024-08-21 23:35:26 +0700

    configure caching by go-redis

---
+++ app/service/redis/init.go
@@ -0,0 +1,98 @@
+package redis
+
+import (
+	"context"
+	"encoding/json"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+)
+
+type ClientType struct {
+	RedisClient *redis.Client
+}
+
+var (
+	Client   *ClientType
+	ctxRedis = context.Background()
+)
+
+func InitRedis(address string) {
+	Client = &ClientType{
+		RedisClient: redis.NewClient(&redis.Options{
+			Addr:     address,
+			Password: "", // no password set
+			DB:       0,  // use default DB
+		}),
+	}
+}
+func (client *ClientType) Set(ctx context.Context, key string, value interface{}, duration time.Duration) error {
+	dataValue, err := json.Marshal(value)
+	if err != nil {
+		return err
+	}
+	setValue := client.RedisClient.Set(ctx, key, dataValue, duration)
+	if setValue.Err() != nil {
+		return setValue.Err()
+	}
+	return nil
+}
+
+func (client *ClientType) get(ctx context.Context, key string, result interface{}) error {
+	valueRe := client.RedisClient.Get(ctx, key)
+	if valueRe.Err() != nil {
+		return valueRe.Err()
+	}
+	err := json.Unmarshal([]byte(valueRe.Val()), result)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (client *ClientType) SetWithBackground(key string, value interface{}, duration time.Duration) error {
+	dataValue, err := json.Marshal(value)
+	if err != nil {
+		return err
+	}
+	cmdRes := client.RedisClient.Set(ctxRedis, key, dataValue, duration)
+	if cmdRes.Err() != nil {
+		return cmdRes.Err()
+	}
+	return nil
+}
+
+func (client *ClientType) GetWithBackground(key string, result interface{}) error {
+	cmdRes := client.RedisClient.Get(ctxRedis, key)
+	if cmdRes.Err() != nil {
+		return cmdRes.Err()
+	}
+	err := json.Unmarshal([]byte(cmdRes.Val()), result)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (client *ClientType) RemoveCacheByKey(key string) error {
+	cmdRes := client.RedisClient.Del(ctxRedis, key)
+	if cmdRes.Err() != nil {
+		return cmdRes.Err()
+	}
+	return nil
+}
+
+func (client *ClientType) RemoveCacheBySubString(stringPattern string) error {
+	cmdRes := client.RedisClient.Keys(ctxRedis, stringPattern)
+	if cmdRes.Err() != nil {
+		return cmdRes.Err()
+	}
+	for _, key := range cmdRes.Val() {
+		err := client.RemoveCacheByKey(key)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
--- go.mod
+++ go.mod
@@ -21,8 +21,10 @@ require (
 	github.com/aead/poly1305 v0.0.0-20180717145839-3fee0db0b635 // indirect
 	github.com/bytedance/sonic v1.11.6 // indirect
 	github.com/bytedance/sonic/loader v0.1.1 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/cloudwego/base64x v0.1.4 // indirect
 	github.com/cloudwego/iasm v0.2.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
@@ -43,6 +45,7 @@ require (
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/redis/go-redis/v9 v9.6.1 // indirect
 	github.com/rogpeppe/go-internal v1.12.0 // indirect
 	github.com/sagikazarmark/locafero v0.4.0 // indirect
 	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
