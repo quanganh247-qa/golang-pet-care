63e2c9057446a5a5efe9b67844d33cce1831afed
quang-anh-ute <21110752@student.hcmute.edu.vn>
2024-12-05 20:38:13 +0700

    get product by id

--- app/api/products/product.controller.go
+++ app/api/products/product.controller.go
@@ -2,6 +2,7 @@ package products
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"github.com/quanganh247-qa/go-blog-be/app/util"
@@ -9,6 +10,7 @@ import (
 
 type ProductControllerInterface interface {
 	GetProducts(c *gin.Context)
+	GetProductByID(c *gin.Context)
 }
 
 func (controller *ProductController) GetProducts(c *gin.Context) {
@@ -26,4 +28,21 @@ func (controller *ProductController) GetProducts(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, products)
+}
+
+func (controller *ProductController) GetProductByID(c *gin.Context) {
+	id := c.Param("product_id")
+	productID, er := strconv.ParseInt(id, 10, 64)
+	if er != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": er.Error()})
+		return
+	}
+
+	product, err := controller.service.GetProductByID(c, productID)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, product)
+
 }
